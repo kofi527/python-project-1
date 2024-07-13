c5450f1cfaa6c5016e22a9b0b43e9cf019748699
kofi527 <63665356+kofi527@users.noreply.github.com>
2024-07-13 06:58:46 -0700

    new update

--- pythonproject.py
+++ pythonproject.py
@@ -1,1 +1,16 @@
 print ("hello world")
+
+from tkinter import *
+import ast
+root =Tk()
+i=0
+def get_number(num):
+    global i
+    display.insert(i,num)
+    i+=1
+
+def get_operation(operator):
+    global i
+    length =len(operator)
+    display.inert(i,operator)
+    i+=length
